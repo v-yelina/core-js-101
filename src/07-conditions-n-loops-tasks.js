dcf69e4650b9e281ca1ada32aa1faf720cfdbb73
v-yelina <heytridvaodin@gmail.com>
2022-07-26 22:10:20 +0200

    feat: finish conditions tasks

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -478,8 +478,72 @@ function getMatrixProduct(m1, m2) {
  *    [    ,   ,    ]]
  *
  */
-function evaluateTicTacToePosition(/* position */) {
-  throw new Error('Not implemented');
+function evaluateTicTacToePosition(position) {
+  if (
+    position[0][0] !== undefined &&
+    position[0][0] === position[1][0] &&
+    position[1][0] === position[2][0] &&
+    position[2][0] === position[0][0]
+  ) {
+    return position[0][0];
+  }
+  if (
+    position[0][1] !== undefined &&
+    position[0][1] === position[1][1] &&
+    position[1][1] === position[2][1] &&
+    position[2][1] === position[0][1]
+  ) {
+    return position[0][1];
+  }
+  if (
+    position[0][2] !== undefined &&
+    position[0][2] === position[1][2] &&
+    position[1][2] === position[2][2] &&
+    position[2][2] === position[0][2]
+  ) {
+    return position[0][2];
+  }
+  if (
+    position[0][0] !== undefined &&
+    position[0][0] === position[1][1] &&
+    position[1][1] === position[2][2] &&
+    position[2][2] === position[0][0]
+  ) {
+    return position[0][0];
+  }
+  if (
+    position[0][2] !== undefined &&
+    position[0][2] === position[1][1] &&
+    position[1][1] === position[2][0] &&
+    position[2][0] === position[0][2]
+  ) {
+    return position[0][2];
+  }
+  if (
+    position[0][0] !== undefined &&
+    position[0][0] === position[0][1] &&
+    position[0][0] === position[0][2] &&
+    position[0][1] === position[0][2]
+  ) {
+    return position[0][0];
+  }
+
+  if (
+    position[1][0] !== undefined &&
+    position[1][0] === position[1][1] &&
+    position[1][0] === position[1][2] &&
+    position[1][1] === position[1][2]
+  ) {
+    return position[1][0];
+  }
+  if (
+    position[2][0] !== undefined &&
+    position[2][0] === position[2][1] &&
+    position[2][0] === position[2][2] &&
+    position[2][1] === position[2][2]
+  ) {
+    return position[2][0];
+  }
 }
 
 module.exports = {
